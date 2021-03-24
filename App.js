fb1b448fb0b523ef25482e825c3cb7e2f87658e1
Shrif Rai <sr1908@g.rit.edu>
2021-03-24 18:52:41 -0400

    Implemented login screen UI

--- App.js
+++ App.js
@@ -1,22 +1,50 @@
 // By Shrif Rai and Scott B. Shaffer
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Image, Button, SafeAreaView, Alert } from 'react-native';
 
-export default function App() {
+// main function/app launcher
+export default function App() { 
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null);
+  console.log("App executed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={require("./assets/new-icon.png")}/>
+      <Text style={styles.welcomeTxt}>This is a password manager app!</Text>
+      <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Email..." 
+            placeholderTextColor="#003f5c"
+            secureTextEntry = {false}
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Password..." 
+            placeholderTextColor="#003f5c"
+            secureTextEntry = {true}
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1, 
-    backgroundColor: '#fff',
+    backgroundColor: '#404cd9',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
