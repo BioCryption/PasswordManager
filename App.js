e210e11d2a357d6f0fd8cf1f5fd6e3f6e1171a30
Shrif Rai <sr1908@g.rit.edu>
2021-04-14 22:57:26 -0400

    Fixed import error for screens

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen } from '/Users/shrifrai/VScodeProjects/PasswordManager/screens'
+import { HomeScreen, LoginScreen, RegistrationScreen } from './screens'
 import {decode, encode} from 'base-64'
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
