6abd2df48da4a798238c4f02c3b4916f5e3227c8
Shrif Rai <sr1908@g.rit.edu>
2021-04-20 19:28:59 -0400

    added home screen functionality
    to-do: fix login

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { HomeScreen, LoginScreen, RegistrationScreen } from './screens'
 import {decode, encode} from 'base-64'
+import { firebase } from './src/firebase/config'
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
@@ -15,6 +16,33 @@ export default function App() {
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
 
+  if (loading) {	
+    return (	
+      <></>	
+    )	
+  }
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []);
+  
   return (
     <NavigationContainer>
       <Stack.Navigator>
